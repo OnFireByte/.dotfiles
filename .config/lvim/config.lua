a820727d9279fdef832c96d700c8cd3e74a64622
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2022-08-30 18:00:17 +0700

    add hop, goto to lvim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,4 +1,4 @@
---[[
+ --[[
 lvim is the global options object
 
 Linters should be
@@ -65,18 +65,18 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+    "bash",
+    "c",
+    "javascript",
+    "json",
+    "lua",
+    "python",
+    "typescript",
+    "tsx",
+    "css",
+    "rust",
+    "java",
+    "yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -157,14 +157,37 @@ lvim.builtin.treesitter.highlight.enabled = true
 --   },
 -- }
 
--- Additional Plugins
--- lvim.plugins = {
---     {"folke/tokyonight.nvim"},
---     {
---       "folke/trouble.nvim",
---       cmd = "TroubleToggle",
---     },
--- }
+-- Additional plugins
+lvim.plugins = { {
+    'phaazon/hop.nvim',
+    branch = 'v2', -- optional but strongly recommended:
+    config = function()
+        -- you can configure Hop the way you like here; see :h hop-config
+        require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+        vim.api.nvim_set_keymap("n", "f", ":HopWord<cr>", { silent = true })
+        vim.api.nvim_set_keymap("n", "F", ":HopPattern<cr>", { silent = true })
+    end
+},
+    {
+        "rmagatti/goto-preview",
+        config = function()
+            require('goto-preview').setup {
+                width = 120; -- Width of the floating window
+                height = 25; -- Height of the floating window
+                default_mappings = true; -- Bind default mappings
+                debug = false; -- Print debug information
+                opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
+                post_open_hook = nil -- A function taking two arguments, a buffer and a window to be ran as a hook.
+                -- You can use "default_mappings = true" setup option
+                -- Or explicitly set keybindings
+                -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+                -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+                -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+            }
+        end
+    },
+}
+
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- vim.api.nvim_create_autocmd("BufEnter", {
@@ -179,3 +202,8 @@ lvim.builtin.treesitter.highlight.enabled = true
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
+
+-- tab indent
+vim.opt.showtabline = 4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
